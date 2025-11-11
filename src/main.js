4b9610f04ae6e821c13679cf56f0a270c6111787
otto@dccia.ua.es <otto@dccia.ua.es>
2025-11-11 09:10:23 +0100

    añadido Colada para time-travel debugging

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from './App.vue'
 import router from './router/index'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' 
+import Colada, { PiniaColadaPlugin } from 'colada-plugin';
 import "./assets/styles.css"
 
 
 const app = createApp(App)
+app.use(router)
+
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
-
-app.use(router)
 app.use(pinia)
+pinia.use(PiniaColadaPlugin)
+app.use(Colada)
 app.mount('#app')
